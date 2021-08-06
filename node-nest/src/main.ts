060c883b1a047d3e535fd2d5b9882a8cb6520183
Paulo Salvatore <salvatoregames@gmail.com>
2021-08-06 12:50:29 -0300

    feat(error): implement `NotFoundInterceptor`

--- node-nest/src/main.ts
+++ node-nest/src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NotFoundInterceptor } from './interceptors/not-found.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Pipes
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -12,6 +14,9 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  // Interceptors
+  app.useGlobalInterceptors(new NotFoundInterceptor());
 
   await app.listen(3000);
 }
