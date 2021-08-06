b9a34c5d5f3f157708f6d9d9edc0f0772e0806ed
Paulo Salvatore <salvatoregames@gmail.com>
2021-08-06 10:03:38 -0300

    chore: cleanup code

--- node-nest/src/app.controller.ts
+++ node-nest/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,10 +8,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Post()
-  createMensagem() {
-    this.appService.createHello();
   }
 }
--- node-nest/src/app.service.ts
+++ node-nest/src/app.service.ts
@@ -2,13 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  nome = 'Paulo';
-
-  createHello() {
-    this.nome = 'Salvatore';
-  }
-
   getHello(): string {
-    return this.nome;
+    return 'Hello World';
   }
 }
