0eb0a865086476e1394c758d7d91b4b900f85f46
Paulo Salvatore <salvatoregames@gmail.com>
2021-08-07 11:28:07 -0300

    feat(prisma): create service

--- node-nest/src/app.module.ts
+++ node-nest/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ColaboradoresModule } from './colaboradores/colaboradores.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [ColaboradoresModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
