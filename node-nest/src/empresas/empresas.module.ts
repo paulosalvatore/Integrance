d9df699eb76b8a16d3b0fef3739fb33ad9441f69
Paulo Salvatore <salvatoregames@gmail.com>
2021-08-14 13:00:27 -0300

    feat(empresas): implement service (WIP)

--- node-nest/src/empresas/empresas.module.ts
+++ node-nest/src/empresas/empresas.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EmpresasService } from './empresas.service';
 import { EmpresasController } from './empresas.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [EmpresasController],
-  providers: [EmpresasService],
+  providers: [EmpresasService, PrismaService],
 })
 export class EmpresasModule {}
--- node-nest/src/empresas/empresas.service.ts
+++ node-nest/src/empresas/empresas.service.ts
@@ -1,26 +1,87 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { EntityNotFoundError } from 'src/errors/entity-not-found.error';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateEmpresaDto } from './dto/create-empresa.dto';
 import { UpdateEmpresaDto } from './dto/update-empresa.dto';
 
 @Injectable()
 export class EmpresasService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createEmpresaDto: CreateEmpresaDto) {
-    return 'This action adds a new empresa';
+    const colaboradores = createEmpresaDto.colaboradores;
+    // (também é possível fazer via desconstrução de objeto):
+    // const { colaboradores } = createEmpresaDto;
+
+    type CreateColaboradores =
+      Prisma.ColaboradoresCreateNestedManyWithoutEmpresasInput;
+
+    const createColaboradores: CreateColaboradores = colaboradores
+      ? { createMany: { data: colaboradores } }
+      : undefined;
+
+    const data: Prisma.EmpresasCreateInput = {
+      ...createEmpresaDto,
+      colaboradores: createColaboradores,
+    };
+
+    return this.prisma.empresas.create({
+      data,
+      include: {
+        colaboradores: true,
+      },
+    });
   }
 
   findAll() {
-    return `This action returns all empresas`;
+    return this.prisma.empresas.findMany({
+      include: {
+        colaboradores: {
+          select: { nome: true },
+        },
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} empresa`;
+  async findOne(id: number) {
+    const data = await this.prisma.empresas
+      .findUnique({
+        where: { id },
+        rejectOnNotFound: true,
+      })
+      .catch(this.handleDatabaseErrors);
+
+    return data;
   }
 
   update(id: number, updateEmpresaDto: UpdateEmpresaDto) {
-    return `This action updates a #${id} empresa`;
+    /*const data: Prisma.EmpresasUpdateInput = {
+      ...updateEmpresaDto,
+    };
+
+    return this.prisma.empresas
+      .update({
+        where: { id },
+        data,
+      })
+      .catch(this.handleDatabaseErrors);*/
   }
 
   remove(id: number) {
-    return `This action removes a #${id} empresa`;
+    return this.prisma.empresas
+      .delete({
+        where: { id },
+      })
+      .catch(this.handleDatabaseErrors);
+  }
+
+  private handleDatabaseErrors(error: PrismaClientKnownRequestError): Error {
+    if (error.code === 'P2025' || error.name === 'NotFoundError') {
+      throw new EntityNotFoundError('Empresa não encontrado.');
+    }
+
+    throw error;
   }
 }
