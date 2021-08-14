64cfc8bfb2c85b1e839b593a84056054454c6c2d
Paulo Salvatore <salvatoregames@gmail.com>
2021-08-14 12:34:28 -0300

    feat(empresas): implement entity and dto

--- node-nest/src/empresas/dto/create-empresa.dto.ts
+++ node-nest/src/empresas/dto/create-empresa.dto.ts
@@ -1,1 +1,15 @@
-export class CreateEmpresaDto {}
+import { Type } from 'class-transformer';
+import { IsArray, IsOptional, IsString, ValidateNested } from 'class-validator';
+import { CreateColaboradorDto } from 'src/colaboradores/dto/create-colaborador.dto';
+import { Empresa } from '../entities/empresa.entity';
+
+export class CreateEmpresaDto implements Empresa {
+  @IsString()
+  razaoSocial: string;
+
+  @IsArray()
+  @ValidateNested()
+  @Type(() => CreateColaboradorDto)
+  @IsOptional()
+  colaboradores?: CreateColaboradorDto[];
+}
--- node-nest/src/empresas/entities/empresa.entity.ts
+++ node-nest/src/empresas/entities/empresa.entity.ts
@@ -1,1 +1,9 @@
-export class Empresa {}
+import { Colaborador } from 'src/colaboradores/entities/colaborador.entity';
+
+export class Empresa {
+  id?: number;
+
+  razaoSocial: string;
+
+  colaboradores?: Colaborador[];
+}
