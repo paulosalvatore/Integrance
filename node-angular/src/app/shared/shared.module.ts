8ff9a292430ac5596b1f8225027c9fd31d7628f5
Paulo Salvatore <salvatoregames@gmail.com>
2021-08-19 11:46:33 -0300

    feat(card): create component

--- node-angular/src/app/shared/shared.module.ts
+++ node-angular/src/app/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CardComponent],
   imports: [CommonModule],
+  exports: [CardComponent],
 })
 export class SharedModule {}
