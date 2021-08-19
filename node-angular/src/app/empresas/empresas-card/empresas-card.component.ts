f2e2f5d207032744e0f06b35f99a00437f96e999
Paulo Salvatore <salvatoregames@gmail.com>
2021-08-19 12:34:10 -0300

    refactor(empresas): extract card component

---
+++ node-angular/src/app/empresas/empresas-card/empresas-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empresas-card',
+  templateUrl: './empresas-card.component.html',
+  styleUrls: ['./empresas-card.component.sass'],
+})
+export class EmpresasCardComponent {}
--- node-angular/src/app/empresas/empresas.module.ts
+++ node-angular/src/app/empresas/empresas.module.ts
@@ -3,9 +3,14 @@ import { CommonModule } from '@angular/common';
 import { EmpresasViewComponent } from './empresas-view/empresas-view.component';
 import { EmpresasSectionComponent } from './empresas-section/empresas-section.component';
 import { SharedModule } from '../shared/shared.module';
+import { EmpresasCardComponent } from './empresas-card/empresas-card.component';
 
 @NgModule({
-  declarations: [EmpresasViewComponent, EmpresasSectionComponent],
+  declarations: [
+    EmpresasViewComponent,
+    EmpresasSectionComponent,
+    EmpresasCardComponent,
+  ],
   imports: [CommonModule, SharedModule],
 })
 export class EmpresasModule {}
