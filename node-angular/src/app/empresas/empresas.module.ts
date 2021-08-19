51513db4f59a5446be035ae6350b5487e3ab3a83
Paulo Salvatore <salvatoregames@gmail.com>
2021-08-19 11:58:52 -0300

    feat(empresas): implement empresas-card

--- node-angular/src/app/empresas/empresas.module.ts
+++ node-angular/src/app/empresas/empresas.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmpresasViewComponent } from './empresas-view/empresas-view.component';
 import { EmpresasSectionComponent } from './empresas-section/empresas-section.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [EmpresasViewComponent, EmpresasSectionComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
 })
 export class EmpresasModule {}
